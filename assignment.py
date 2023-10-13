3e2ba81ba40ef3b9521380d4093c21f449d874a5
JacobLorSHH <144691606+JacobLorSHH@users.noreply.github.com>
2023-10-13 22:56:02 +0000

    Assignment 2

--- assignment.py
+++ assignment.py
@@ -1,1 +1,11 @@
+import math
 
+print('Your output most be a number!')
+try:
+    radius = float(input('What is the radius?'))
+    area = (radius ** 2) * math.pi
+    circumference = (radius * 2) * math.pi
+    print('area:',area)
+    print("circumference:",circumference)
+except ValueError:
+    print('Not a Number')

71f732f8ef25e794086d714c328054d0169e63e9
JacobLorSHH <144691606+JacobLorSHH@users.noreply.github.com>
2023-10-15 19:39:44 +0000

    Python works

--- assignment.py
+++ assignment.py
@@ -2,7 +2,7 @@ import math
 
 print('Your output must be a number!')
 try:
-    radius = float(input('What is the radius?'))
+    radius = float(input('What is the radius? '))
     area = (radius ** 2) * math.pi
     circumference = (radius * 2) * math.pi
     print('The area of your circle is:',area)
